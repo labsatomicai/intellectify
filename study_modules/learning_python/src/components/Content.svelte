81768bc7bad3e9092444912cbef9486627c4f055
Marco Tulio Duenas <marcot.dns@gmail.com>
2024-03-04 18:42:03 -0300

    Creating study_modules folder, changing topic of firts module

---
+++ study_modules/learning_python/src/components/Content.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { fly } from 'svelte/transition'
+</script>
+
+<div class="python-history" in:fly>
+    <math>
+        H(s) = ∫<sub>0</sub><sup>∞</sup> e<sup>-st</sup> h(t) dt
+    </math> 
+</div>
+
+<style>
+    .python-history{
+        text-align: center;
+    }
+</style>
--- study_modules/learning_python/src/components/Header.svelte
+++ study_modules/learning_python/src/components/Header.svelte
@@ -6,12 +6,15 @@
         <img class="logo" width="180px" src="/img/neurahub_logo_no_bg.png" alt="">
     </div>
     <div class="game-title">
-       <h1>Learning about</h1> 
-       <img width="200px" src="/img/py_logo.png" alt="">
+        <h1>Mathematicas in everyday life: An interactive exploration</h1>
     </div>
 </header>
 
 <style>
+    header{
+        height: 75vh;
+    }
+
     .logo-container{
         padding-left: 10rem;
     }
@@ -27,5 +30,4 @@
         justify-content: center;
         align-items: center;
     }
-
 </style>
